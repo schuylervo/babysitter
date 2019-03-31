80030b50f9aceb7f667c1b14363285e2bee5b984
Schuyler <svonoeyen@gmail.com>
2019-03-30 23:48:00 -0400

    family A two pay rates

--- src/main/java/babykata/paythesitter/PayCalculator.java
+++ src/main/java/babykata/paythesitter/PayCalculator.java
@@ -4,10 +4,15 @@ package babykata.paythesitter;
 public class PayCalculator {
 	
 	
-	public int pay(int hoursWorked) {
+	public int payFromFamilyABetween5pmAnd11pm(int hoursWorked) {
 		int payRate = 15;
 		int wageEarned = payRate * hoursWorked;
 		return wageEarned;
 	}
-    
+	
+	public int payFromFamilyABetween11pmAnd4am(int hoursWorked) {
+		int payRate = 20;
+		int wageEarned = payRate * hoursWorked;
+		return wageEarned;
+	}
 }
--- src/test/java/babykata/paythesitter/PayCalculatorTest.java
+++ src/test/java/babykata/paythesitter/PayCalculatorTest.java
@@ -6,9 +6,22 @@ import org.junit.Test;
 
 public class PayCalculatorTest {
 	@Test
-	public void WhenSittingForFamilyABefore11PayIs15PerHour () {
+	public void WhenSittingForFamilyABetween5pmAnd11pmPayIs15PerHour () {
 		PayCalculator payCalculator = new PayCalculator();
-		assertEquals(15, payCalculator.pay(1));
+		assertEquals(0, payCalculator.payFromFamilyABetween5pmAnd11pm(0));
+		assertEquals(15, payCalculator.payFromFamilyABetween5pmAnd11pm(1));
+		assertEquals(30, payCalculator.payFromFamilyABetween5pmAnd11pm(2));
+		assertEquals(45, payCalculator.payFromFamilyABetween5pmAnd11pm(3));
+		assertEquals(90, payCalculator.payFromFamilyABetween5pmAnd11pm(6));
+	}
+	
+	@Test
+	public void WhenSittingForFamilyABetween11pmAnd4amPayIs20PerHour () {
+		PayCalculator payCalculator = new PayCalculator();
+		assertEquals(0, payCalculator.payFromFamilyABetween11pmAnd4am(0));
+		assertEquals(20, payCalculator.payFromFamilyABetween11pmAnd4am(1));
+		assertEquals(40, payCalculator.payFromFamilyABetween11pmAnd4am(2));
+		assertEquals(100, payCalculator.payFromFamilyABetween11pmAnd4am(5));
 	}
 }    
     
