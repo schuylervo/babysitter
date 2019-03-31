aead9c3196e6eab184ec48510ca8f5be599d264c
Schuyler <svonoeyen@gmail.com>
2019-03-31 00:10:09 -0400

    pay function now takes start time and end time as parameters

--- src/main/java/babykata/paythesitter/PayCalculator.java
+++ src/main/java/babykata/paythesitter/PayCalculator.java
@@ -3,14 +3,21 @@ package babykata.paythesitter;
 
 public class PayCalculator {
 	
-	
-	public int payFromFamilyABetween5pmAnd11pm(int hoursWorked) {
+	public int payFromFamilyABetween5pmAnd11pm(int startTime, int endTime) {
+		int hoursWorked = endTime - startTime;
 		int payRate = 15;
 		int wageEarned = payRate * hoursWorked;
 		return wageEarned;
 	}
 	
-	public int payFromFamilyABetween11pmAnd4am(int hoursWorked) {
+	public int payFromFamilyABetween11pmAnd4am(int startTime, int endTime) {
+		if (startTime <=4) {
+			startTime += 12;
+		}
+		if (endTime <=4) {
+			endTime += 12;
+		}
+		int hoursWorked = endTime - startTime;
 		int payRate = 20;
 		int wageEarned = payRate * hoursWorked;
 		return wageEarned;
--- src/test/java/babykata/paythesitter/PayCalculatorTest.java
+++ src/test/java/babykata/paythesitter/PayCalculatorTest.java
@@ -8,20 +8,24 @@ public class PayCalculatorTest {
 	@Test
 	public void WhenSittingForFamilyABetween5pmAnd11pmPayIs15PerHour () {
 		PayCalculator payCalculator = new PayCalculator();
-		assertEquals(0, payCalculator.payFromFamilyABetween5pmAnd11pm(0));
-		assertEquals(15, payCalculator.payFromFamilyABetween5pmAnd11pm(1));
-		assertEquals(30, payCalculator.payFromFamilyABetween5pmAnd11pm(2));
-		assertEquals(45, payCalculator.payFromFamilyABetween5pmAnd11pm(3));
-		assertEquals(90, payCalculator.payFromFamilyABetween5pmAnd11pm(6));
+		assertEquals(0, payCalculator.payFromFamilyABetween5pmAnd11pm(5, 5));
+		assertEquals(15, payCalculator.payFromFamilyABetween5pmAnd11pm(6, 7));
+		assertEquals(30, payCalculator.payFromFamilyABetween5pmAnd11pm(8, 10));
+		assertEquals(45, payCalculator.payFromFamilyABetween5pmAnd11pm(7, 10));
+		assertEquals(90, payCalculator.payFromFamilyABetween5pmAnd11pm(5, 11));
 	}
 	
 	@Test
 	public void WhenSittingForFamilyABetween11pmAnd4amPayIs20PerHour () {
 		PayCalculator payCalculator = new PayCalculator();
-		assertEquals(0, payCalculator.payFromFamilyABetween11pmAnd4am(0));
-		assertEquals(20, payCalculator.payFromFamilyABetween11pmAnd4am(1));
-		assertEquals(40, payCalculator.payFromFamilyABetween11pmAnd4am(2));
-		assertEquals(100, payCalculator.payFromFamilyABetween11pmAnd4am(5));
+		assertEquals(0, payCalculator.payFromFamilyABetween11pmAnd4am(2, 2));
+		assertEquals(20, payCalculator.payFromFamilyABetween11pmAnd4am(11, 12));
+		assertEquals(20, payCalculator.payFromFamilyABetween11pmAnd4am(12, 1));
+		assertEquals(20, payCalculator.payFromFamilyABetween11pmAnd4am(1, 2));
+		assertEquals(40, payCalculator.payFromFamilyABetween11pmAnd4am(12, 2));
+		assertEquals(100, payCalculator.payFromFamilyABetween11pmAnd4am(11,4));
 	}
+	
+	
 }    
     
