ea3a6b3c32db83e41492fd1edf852be28549ef74
Schuyler <svonoeyen@gmail.com>
2019-04-03 17:17:49 -0400

    Family A pay calculator now working for start times before and after 11pm and end times before and after midnight

--- src/main/java/babykata/paythesitter/PayCalculator.java
+++ src/main/java/babykata/paythesitter/PayCalculator.java
@@ -16,7 +16,7 @@ public class PayCalculator {
 	
 	public int payFromFamilyABetween11pmAnd4am(LocalTime startTime, LocalTime endTime) {
 		
-		if (startTime.isAfter(LocalTime.of(16, 59, 59)) && endTime.isBefore(LocalTime.of(4, 0, 1))) {
+		if (startTime.isAfter(LocalTime.of(22, 59, 59)) && endTime.isBefore(LocalTime.of(4, 0, 1))) {
 			startTime = startTime.minusHours(12);
 			endTime = endTime.plusHours(12);
 		}
@@ -27,4 +27,79 @@ public class PayCalculator {
 		int wageEarned = payRate * hoursWorked;
 		return wageEarned;
 	}
+	
+	public int payFromFamilyABetween5pmAnd4am(LocalTime startTime, LocalTime endTime) {
+		
+		if (startTime.isAfter(LocalTime.of(16, 59, 59)) && endTime.isBefore(LocalTime.of(23, 0, 1)) && endTime.isAfter(LocalTime.of(16, 59, 59))) {
+			
+			long longHoursWorked = ChronoUnit.HOURS.between(startTime, endTime);
+			int hoursWorked = (int)longHoursWorked;
+			int payRate = 15;
+			int wageEarned = payRate * hoursWorked;
+			return wageEarned;
+			
+		} else if (startTime.isAfter(LocalTime.of(16, 59, 59)) && endTime.isAfter(LocalTime.of(23, 0, 0))) {	
+			
+			// note: this method will return wages earned between 5:00 p.m. and 11:00 p.m. in whole number increments
+			// wages earned between 11pm and 11:59:59pm will be zero because it would be a fraction of an hour.
+			LocalTime elevenOClock = LocalTime.of(23, 0, 0);
+			long longHoursWorkedBeforeEleven = ChronoUnit.HOURS.between(startTime, elevenOClock);
+			int hoursWorkedBeforeEleven = (int)longHoursWorkedBeforeEleven;
+			int payRateBeforeEleven = 15;
+			int wageEarnedBeforeEleven = payRateBeforeEleven * hoursWorkedBeforeEleven;
+			
+			return wageEarnedBeforeEleven;
+			/* I wrote the code below, but then realized wages earned between 11pm and just before midnight will equate to zero because sitter worked less than one hr at this wage.
+			long longHoursWorkedBetweenElevenAndMidnight = ChronoUnit.HOURS.between(elevenOClock, endTime);
+			int hoursWorkedBetweenElevenAndMidnight = (int)longHoursWorkedBetweenElevenAndMidnight;
+			int payRateBetweenElevenAndMidnight = 20;
+			int wageEarnedBetweenElevenAndMidnight = payRateBetweenElevenAndMidnight * hoursWorkedBetweenElevenAndMidnight;
+			
+			return wageEarnedBeforeEleven + wageEarnedBetweenElevenAndMidnight;
+			*/
+			
+		} else if (startTime.isAfter(LocalTime.of(16, 59, 59)) && endTime.isBefore(LocalTime.of(4, 0, 1))) {
+			
+			LocalTime elevenAtNight = LocalTime.of(23, 0, 0);
+			
+			long longHoursWorkedBeforeEleven = ChronoUnit.HOURS.between(startTime, elevenAtNight);
+			int hoursWorkedBeforeEleven = (int)longHoursWorkedBeforeEleven;
+			int payRateBeforeEleven = 15;
+			int wageEarnedBeforeEleven = payRateBeforeEleven * hoursWorkedBeforeEleven;
+			
+			LocalTime elevenInMorning = LocalTime.of(11, 0, 0);
+			endTime = endTime.plusHours(12);
+			long longHoursWorkedBetweenElevenAndEndTime = ChronoUnit.HOURS.between(elevenInMorning, endTime);
+			int hoursWorkedBetweenElevenAndEndTime = (int)longHoursWorkedBetweenElevenAndEndTime;
+			int payRateBetweenElevenAndEndTime = 20;
+			int wageEarnedBetweenElevenAndEndTime = payRateBetweenElevenAndEndTime * hoursWorkedBetweenElevenAndEndTime;
+			
+			return wageEarnedBeforeEleven + wageEarnedBetweenElevenAndEndTime;
+			
+		} else if (startTime.isAfter(LocalTime.of(22, 59, 59)) && endTime.isBefore(LocalTime.of(23, 59, 59))) {
+			// if startTime is after 11pm and endTime is before midnight, wage will be zero because hours worked is less than 1;
+			
+			return 0;
+			
+		} else if (startTime.isAfter(LocalTime.of(22, 59, 59)) && endTime.isBefore(LocalTime.of(4, 0, 1))) {
+			
+			startTime = startTime.minusHours(12);
+			endTime = endTime.plusHours(12);
+			long longHoursWorked = ChronoUnit.HOURS.between(startTime, endTime);
+			int hoursWorked = (int)longHoursWorked;
+			int payRate = 20;
+			int wageEarned = payRate * hoursWorked;
+			return wageEarned;
+			
+		} else {
+			
+			long longHoursWorked = ChronoUnit.HOURS.between(startTime, endTime);
+			int hoursWorked = (int)longHoursWorked;
+			int payRate = 20;
+			int wageEarned = payRate * hoursWorked;
+			return wageEarned;
+		}
+		
+	}
+	
 }
--- src/test/java/babykata/paythesitter/PayCalculatorTest.java
+++ src/test/java/babykata/paythesitter/PayCalculatorTest.java
@@ -20,12 +20,12 @@ public class PayCalculatorTest {
 		LocalTime time10pm = LocalTime.of(22, 0, 0);
 		LocalTime time11pm = LocalTime.of(23, 0, 0);
 		assertEquals(0, payCalculator.payFromFamilyABetween5pmAnd11pm(time5pm, time5pm));
-		// less than one hour on the job should resolve to no pay
+		// less than one hour on the job should result in no pay
 		assertEquals(0, payCalculator.payFromFamilyABetween5pmAnd11pm(time5pm, time515pm));
 		assertEquals(15, payCalculator.payFromFamilyABetween5pmAnd11pm(time545pm, time7pm));
 		assertEquals(30, payCalculator.payFromFamilyABetween5pmAnd11pm(time6pm, time8pm));
 		assertEquals(45, payCalculator.payFromFamilyABetween5pmAnd11pm(time7pm, time10pm));
-		// 5 hours and 45 minutes on the job should resolve to 5 hours of pay
+		// 5 hours and 45 minutes on the job should result in 5 hours of pay (round down)
 		assertEquals(75, payCalculator.payFromFamilyABetween5pmAnd11pm(time515pm, time11pm));
 		
 	}
@@ -43,16 +43,47 @@ public class PayCalculatorTest {
 		LocalTime time4am = LocalTime.of(4, 0, 0);
 		assertEquals(0, payCalculator.payFromFamilyABetween11pmAnd4am(midnight, midnight));
 		assertEquals(0, payCalculator.payFromFamilyABetween11pmAnd4am(time1120pm, time1159pm));
-		assertEquals(20, payCalculator.payFromFamilyABetween11pmAnd4am(time11pm, midnight));
 		assertEquals(0, payCalculator.payFromFamilyABetween11pmAnd4am(time1120pm, midnight));
 		assertEquals(0, payCalculator.payFromFamilyABetween11pmAnd4am(time1am, time156am));
 		assertEquals(20, payCalculator.payFromFamilyABetween11pmAnd4am(midnight, time1am));
+		assertEquals(20, payCalculator.payFromFamilyABetween11pmAnd4am(time11pm, midnight));
 		assertEquals(20, payCalculator.payFromFamilyABetween11pmAnd4am(midnight, time156am));
 		assertEquals(40, payCalculator.payFromFamilyABetween11pmAnd4am(time1120pm, time156am));
 		assertEquals(80, payCalculator.payFromFamilyABetween11pmAnd4am(time1120pm, time4am));
 		assertEquals(100, payCalculator.payFromFamilyABetween11pmAnd4am(time11pm, time4am));
 		
 	}
+	
+	@Test
+	public void WhenSittingForFamilyABetween5pmAnd4amPayIs15PerHourUntil11pmAnd20PerHourThereafter () {
+		PayCalculator payCalculator = new PayCalculator();
+		
+		LocalTime time515pm = LocalTime.of(17, 15, 0);
+		LocalTime time845pm = LocalTime.of(20, 45, 0);
+		LocalTime time1059pm = LocalTime.of(22, 59, 0);
+		LocalTime time11pm = LocalTime.of(23, 0, 0);
+		LocalTime time1120pm = LocalTime.of(23, 20, 0);
+		LocalTime time1159pm = LocalTime.of(23, 59, 0);
+		LocalTime time1am = LocalTime.of(1, 0, 0);
+		LocalTime time156am = LocalTime.of(1, 56, 0);
+		LocalTime time4am = LocalTime.of(4, 0, 0);
+		// test times before starting after 5pm and finishing before midnight
+		assertEquals(0, payCalculator.payFromFamilyABetween5pmAnd4am(time1059pm, time1120pm));
+		assertEquals(0, payCalculator.payFromFamilyABetween5pmAnd4am(time1120pm, time1159pm));
+		assertEquals(30, payCalculator.payFromFamilyABetween5pmAnd4am(time845pm, time1059pm));
+		assertEquals(75, payCalculator.payFromFamilyABetween5pmAnd4am(time515pm, time11pm));
+		assertEquals(75, payCalculator.payFromFamilyABetween5pmAnd4am(time515pm, time1120pm));
+		
+		// test times before starting after 5pm and finishing after might
+		assertEquals(70, payCalculator.payFromFamilyABetween5pmAnd4am(time845pm, time1am));
+		assertEquals(70, payCalculator.payFromFamilyABetween5pmAnd4am(time845pm, time156am));
+		assertEquals(130, payCalculator.payFromFamilyABetween5pmAnd4am(time845pm, time4am));
+		assertEquals(40, payCalculator.payFromFamilyABetween5pmAnd4am(time1120pm, time156am));
+	
+		
+	}
+	
+	
 	
 	
 }    
